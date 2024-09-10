3de82b27799f6b2a1bd86aad1f9f304fcaf7dadf
Tony-Villa <tonyvilla77@gmail.com>
2024-09-10 15:49:22 -0700

    fix: wip - about section

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -4,6 +4,6 @@
 </script>
     
 <Card variant="sm" title="About">
-  <p class="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fuga veniam deserunt quaerat, voluptatibus, velit sit animi amet molestiae nihil quas nam ullam, dolorem excepturi porro quibusdam consequuntur explicabo dolor.</p>
+  <p class="text-xl">I'm a full-stack developer with expertise in front end development. As a lifelong learner, one of my favorite things to do is to play with new and old technologies on side projects and always keep an open mind when it comes to building software.</p>
 </Card>
     
