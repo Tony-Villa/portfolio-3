44e304ee2b2904b3e96978dec31c102fdc55810e
Tony-Villa <tonyvilla77@gmail.com>
2024-09-12 11:23:33 -0700

    fix: add anchors to sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,33 +5,40 @@
 	import Hero from "$lib/components/Hero.svelte";
 	import Projects from "$lib/components/Projects.svelte";
 	import Skills from "$lib/components/Skills.svelte";
+	import Navbar from "$lib/components/navbar/Navbar.svelte";
 
 </script>
 
-<div class="h-dvh w-dvw flex flex-col gap-20 px-5 py-2 md:px-20 md:py-10">
-  <section class="h-3/4 min-h-[75%]">
-    <Hero />
-  </section>
-  <section>
-    <About />
-  </section>
-  <section>
-    <Projects />
-  </section>
-  <section class="min-h-[25%]">
-    <Skills />
-  </section>
-  <section>
-    <Experience />
-  </section>
-  <section class="min-h-[1000px]">
-    <Contact />
-  </section>
-  
-</div>
+  <div class="h-dvh flex flex-col gap-20 px-5 py-2 md:px-20 md:py-10">
+    <section class="page-sec h-3/4 min-h-[75%]">
+      <Hero />
+    </section>
+    <section id="about" class="page-sec">
+      <About />
+    </section>
+    <section id="projects" class="page-sec">
+      <Projects />
+    </section>
+    <section id="skills-section" class="page-sec min-h-[25%]">
+      <Skills />
+    </section>
+    <section id="experience" class="page-sec">
+      <Experience />
+    </section>
+    <section id="contact" class="page-sec min-h-[1000px]">
+      <Contact />
+    </section>
+    
+  </div>
+
 
 <style>
   :global(html) {
     background-color: #F8EFD2;
+  }
+
+  .page-sec {
+    scroll-behavior: smooth;
+    scroll-margin-top: 50px;
   }
 </style>
