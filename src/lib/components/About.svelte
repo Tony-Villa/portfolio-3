35e9d66e0eb9ac328d74780fb90145a604bbda8b
Tony-Villa <tonyvilla77@gmail.com>
2024-09-24 11:46:33 -0700

    fix: update about copy

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -4,9 +4,9 @@
 </script>
     
 <Card variant="sm" title="About">
-  <div class="flex flex-col text-xl gap-8">
+  <div class="flex flex-col text-xl gap-5">
     <p>
-      I'm a full-stack developer with a passion for creating intuitive front-end experiences. When I’m not coding professionally, you’ll find me ... coding recreationally. Lifelong learning is my superpower. I’m driven by curiosity and am always striving to improve my skills.
+      I'm a full-stack developer with a passion for creating intuitive front-end experiences. When I’m not coding professionally, you’ll find me ... coding recreationally.
     </p>
 
     <p>
@@ -14,7 +14,7 @@
     </p>
 
     <p>
-      Outside of coding, I dabble in amateur cooking, where I treat each recipe like a problem-solving exercise (with tastier results). My love for hockey has reinforced a sense of teamwork and the importance of precision, both in sports and in development. 
+      Outside of coding, I dabble in amateur cooking, where I treat each recipe like a problem-solving exercise (with tastier results). My love for hockey has reinforced a sense of teamwork and the importance of precision, both in sports and in development. Lifelong learning is my superpower. I’m driven by curiosity and am always striving to improve my skills.
     </p>
   </div>
 </Card>
