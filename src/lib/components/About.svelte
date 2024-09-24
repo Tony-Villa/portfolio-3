9a24df302c5380e589722240311e1bc0c5ceeee8
Tony-Villa <tonyvilla77@gmail.com>
2024-09-23 18:13:24 -0700

    fix: wip about section (needs ending)

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -6,15 +6,15 @@
 <Card variant="sm" title="About">
   <div class="flex flex-col text-xl gap-8">
     <p>
-      I'm a full-stack developer with a passion for creating intuitive front-end experiences. When I’m not coding, you’ll find me exploring new tech stacks or finding fresh ways to tackle complex problems in side projects. Lifelong learning is my superpower—whether it's mastering a new framework or refining my debugging process, I’m always driven by curiosity and a love for improvement.
+      I'm a full-stack developer with a passion for creating intuitive front-end experiences. When I’m not coding professionally, you’ll find me ... coding recreationally. Lifelong learning is my superpower. I’m driven by curiosity and am always striving to improve my skills.
     </p>
-  
+
     <p>
-      I became a developer, I spent years working as a filmmaker, where I honed my skills in collaboration, storytelling, and managing cross-functional teams. Much like building software, filmmaking taught me the importance of working together with different experts—from editors to sound designers—to deliver a cohesive product. That same collaborative spirit translates perfectly into software development, where I thrive in environments that bring designers, engineers, and stakeholders together to solve problems as a team.
+      Before I became a developer, I spent years working as a filmmaker where I honed my skills in collaboration, while being a part of a variety of cross functional teams. That same collaborative spirit translates perfectly into software development, where I thrive in environments that bring designers, engineers, and stakeholders together to solve problems as a team.
     </p>
-      
+
     <p>
-      Outside of coding, I dabble in amateur cooking, where I treat each recipe like a problem-solving exercise (with tastier results). My love for hockey has reinforced this sense of teamwork and the importance of precision, both in sports and in development. And of course, my dog has taught me all about patience and adaptability, key traits for any developer facing a tough bug or an unexpected challenge!
+      Outside of coding, I dabble in amateur cooking, where I treat each recipe like a problem-solving exercise (with tastier results). My love for hockey has reinforced a sense of teamwork and the importance of precision, both in sports and in development. 
     </p>
   </div>
 </Card>
